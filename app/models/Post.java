ef424f2b4e47058f29f5a8b6bddc578bca8e1004
Sel <451580679@qq.com>
2014-03-17 23:46:58 +0800

    version 2
    add the post and comment.

---
+++ app/models/Post.java
@@ -0,0 +1,46 @@
+package models;
+
+import java.util.ArrayList;
+import java.util.Date;
+import java.util.List;
+
+import javax.persistence.CascadeType;
+import javax.persistence.Entity;
+import javax.persistence.Lob;
+import javax.persistence.ManyToOne;
+import javax.persistence.OneToMany;
+
+import org.hibernate.annotations.Cascade;
+
+import play.db.jpa.Model;
+
+@Entity
+public class Post extends Model {
+
+	public String title;
+	public Date postedAt;
+	
+	@Lob
+	public String content;
+	
+	@ManyToOne
+	public User author;
+	
+	@OneToMany(mappedBy="post", cascade=CascadeType.ALL)
+	public List<Comment> comments;
+
+	public Post(String title, String content, User author) {
+		this.title = title;
+		this.content = content;
+		this.author = author;
+		this.postedAt = new Date();
+		this.comments = new ArrayList<Comment>();
+	}
+	
+	public Post addComment(String author, String content) {
+		Comment newComment = new Comment(author, content, this).save();
+		this.comments.add(newComment);
+		this.save();
+		return this;
+	}
+}
--- test/BasicTest.java
+++ test/BasicTest.java
@@ -1,13 +1,48 @@
 import org.junit.*;
+
 import java.util.*;
+
 import play.test.*;
 import models.*;
 
 public class BasicTest extends UnitTest {
+	
+	@Before
+	public void setup() {
+		Fixtures.deleteDatabase();
+	}
 
     @Test
-    public void aVeryImportantThingToTest() {
-        assertEquals(2, 1 + 1);
+    public void useTheCommentsRelation() {
+        // Create a new user and save it
+        User bob = new User("bob@gmail.com", "secret", "Bob").save();
+     
+        // Create a new post
+        Post bobPost = new Post("Hello world", "My first post", bob).save();
+     
+        // Post a first comment
+        bobPost.addComment("Jeff", "Nice post");
+        bobPost.addComment("Tom", "I knew that !");
+     
+        // Count things
+        assertEquals(1, User.count());
+        assertEquals(1, Post.count());
+        assertEquals(2, Comment.count());
+     
+        // Retrieve Bob's post
+        bobPost = Post.find("byAuthor", bob).first();
+        assertNotNull(bobPost);
+     
+        // Navigate to comments
+        assertEquals(2, bobPost.comments.size());
+        assertEquals("Jeff", bobPost.comments.get(0).author);
+        
+        // Delete the post
+        bobPost.delete();
+        
+        // Check that all comments have been deleted
+        assertEquals(1, User.count());
+        assertEquals(0, Post.count());
+        assertEquals(0, Comment.count());
     }
-
 }
